1df4ca7c1e2f92799edc0ea3f8ce779693155e53
Yosra Mohsen <yosramohsen2@gmail.com>
2025-11-16 07:28:44 +0200

    add splash image and app fonts

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,5 @@
 class AssetsData {
-  
+  static const String backGround = 'assets/images/splash1.png';
+  static const String diamondLight = 'assets/images/diamond light.png';
+  static const String glitter = 'assets/images/glitter.png';
 }
