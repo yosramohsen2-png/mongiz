a8a6684ff88bd3262ee78f756c232fb8d02ecb8e
Yosra Mohsen <yosramohsen2@gmail.com>
2025-11-16 07:45:59 +0200

    add constans file

---
+++ lib/constans.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const List<Color> kPrimaryGradientColors = [
+  Color(0xFF5391ED),
+  Color(0xFFC4B0FF),
+];
+const List<Color> kCardGradientColors = [Color(0xFF18367B), Color(0xFF7548CF)];
+const List<Color> kButtomGradientColors = [
+  Color(0xFF501794),
+  Color(0xFF3E70A1),
+];
