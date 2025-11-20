4f2e307920529a1e3ffb671a30c0ee0c8a13ff6e
Yosra Mohsen <yosramohsen2@gmail.com>
2025-11-20 12:18:40 +0200

    add plugin flutter assets

--- lib/constans.dart
+++ lib/constans.dart
@@ -1,29 +1,8 @@
-// import 'package:flutter/material.dart';
-
-// const List<Color> kPrimaryGradientColors = [
-//   Color(0xFF5391ED),
-//   Color(0xFFC4B0FF),
-// ];
-// const List<Color> kCardGradientColors = [Color(0xFF18367B), Color(0xFF7548CF)];
-// const List<Color> kButtomGradientColors = [
-//   Color(0xFF501794),
-//   Color(0xFF3E70A1),
-// ];
 import 'package:flutter/material.dart';
 
-// 1. الألوان الثابتة المطلوبة للـ Dark Theme
-// ------------------------------------------------------------------
-
-// اللون الأساسي الداكن للخلفية (Scaffold) ليتناسب مع الـ Dark Theme
-// هذا اللون يمثل 'Black' Theme
 const Color kScaffoldDarkColor = Color(0xFF100B20);
 
-// اللون الأساسي للتطبيق (Primary Color) الذي سيستخدم في الـ Theme
-// تم اختياره كأول لون في الـ Gradient ليكون لون هوية التطبيق
 const Color kAppPrimaryColor = Color(0xFF5391ED);
-
-// 2. تدرجات الألوان (الـ Gradients) التي طلبها المستخدم
-// ------------------------------------------------------------------
 
 const List<Color> kPrimaryGradientColors = [
   Color(0xFF5391ED),
---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for assetsImagesAst
+  /// assets/images/ast.png
+  static const String assetsImagesAst = "assets/images/ast.png";
+
+  /// Assets for assetsImagesDiamond light
+  /// assets/images/diamond light.png
+  static const String assetsImagesDiamondLight =
+      "assets/images/diamond light.png";
+
+  /// Assets for assetsImagesGlitter
+  /// assets/images/glitter.png
+  static const String assetsImagesGlitter = "assets/images/glitter.png";
+
+  /// Assets for assetsImagesSplash1
+  /// assets/images/splash1.png
+  static const String assetsImagesSplash1 = "assets/images/splash1.png";
+}
