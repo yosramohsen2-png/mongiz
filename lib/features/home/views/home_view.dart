6aae4470702fd1bde38cc1becd4eb88f34cfc074
Yosra Mohsen <yosramohsen2@gmail.com>
2025-11-21 15:09:31 +0200

    add home view body

--- lib/features/home/views/home_view.dart
+++ lib/features/home/views/home_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:mongiz/features/home/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
   static const String routeName = 'homeView';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return HomeViewBody();
   }
 }
